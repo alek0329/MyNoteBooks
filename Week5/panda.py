eb430a450c719a6e34f59854f074e44966f58fa9
Alek0329 <alek0329@gmail.com>
2022-03-27 17:45:00 +0200

    'changes'

--- Week5/panda.py
+++
@@ -1,7 +0,0 @@
-import pandas
-filename = "data_extract_from_world_development_indicators_data.csv"
-
-
-
-if __name__ == '__main__':
-    pandas.read_csv(filename)
---
+++ Week6/textcomparer.py
@@ -0,0 +1,17 @@
+class TextComparer:
+
+    def __init__(self, author, title, text):
+
+    def download(url, filename):
+
+    def multi_download():
+
+    def __iter__():
+    
+    def __next__():
+
+    def urllist_generator():
+
+    def avg_vowels(text):
+    
+    def hardest_read():
